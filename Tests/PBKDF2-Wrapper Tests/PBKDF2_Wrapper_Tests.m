3ded0c068a0e057dcb395699edd63a4dee3a0eda
Joey Meyer <jmeyer41@gmail.com>
2014-04-21 16:54:48 +0800

    added Xcode project for unit tests

---
+++ Tests/PBKDF2-Wrapper Tests/PBKDF2_Wrapper_Tests.m
@@ -0,0 +1,34 @@
+//
+//  PBKDF2_Wrapper_Tests.m
+//  PBKDF2-Wrapper Tests
+//
+//  Created by Joey Meyer on 4/21/14.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PBKDF2_Wrapper_Tests : XCTestCase
+
+@end
+
+@implementation PBKDF2_Wrapper_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
