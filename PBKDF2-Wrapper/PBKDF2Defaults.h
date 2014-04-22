33d6190984d9d760bd715993967cb9d55c9ab1d4
Joey Meyer <jmeyer41@gmail.com>
2014-04-22 17:01:29 +0800

    add documenation for defaults

--- PBKDF2-Wrapper/PBKDF2Defaults.h
+++ PBKDF2-Wrapper/PBKDF2Defaults.h
@@ -9,8 +9,27 @@
 #import <Foundation/Foundation.h>
 #import "PBKDF2Configuration.h"
 
+/**
+ The default value for the derived key length, in bytes. (32)
+ */
 extern NSUInteger PBKDF2DefaultDerivedKeyLength;
+
+/**
+ The default value for the delay in milliseconds when calibrating the number of rounds on `PBKDF2Configuration`. (100)
+ */
 extern uint32_t   PBKDF2DefaultDelayInMilliseconds;
+
+/**
+ The default value for the salt length, in bytes. (64)
+ */
 extern NSUInteger PBKDF2DefaultSaltLength;
+
+/**
+ The default value for the number of rounds. (10000)
+ */
 extern NSUInteger PBKDF2DefaultRounds;
+
+/**
+ The default value for the pseudo random function. (PBKDF2PseudoRandomFunctionSHA1)
+ */
 extern PBKDF2PseudoRandomFunction PBKDF2DefaultPseudoRandomFunction;
